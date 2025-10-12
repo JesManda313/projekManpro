4f2b79f3d105cb14b8f69994202c90c66a8f008b
Linda <lindawatisn@gmail.com>
2025-10-12 09:43:25 +0700

    pendaftaran skripsi mhs and jadwal sidang skripsi mhs

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ Route::get('/admin/add-role', function () { return view('superadmin/add-role');}
 // mhs
 Route::get('/mhs/dashboard', function () { return view('mhs/dashboardMhs');});
 Route::get('/mhs/pilihdosbing', function () { return view('mhs/pilihdosbingMhs');});
-Route::get('/mhs/topikdosen', function () { return view('mhs/topikDpsen');});
+Route::get('/mhs/topikdosen', function () { return view('mhs/topikDosen');});
 Route::get('/mhs/sidang-proposal', function () { return view('mhs/sidangProposalMhs');});
 Route::get('/mhs/sidang-skripsi', function () { return view('mhs/sidangSkripsiMhs');});
+Route::get('/mhs/pendaftaran-skripsi', function () { return view('mhs/pendaftaranSkripsiMhs');});
+Route::get('/mhs/jadwal-sidang-skripsi', function () { return view('mhs/jadwalSidangSkripsiMhs');});
 // Route::get('/profile', function () { return view('mhs/profileMhs');});
 Route::get('/profile', function () { return view('mhs/profileMhs');});
 // Route::get('/settings', function () { return view('mhs/settingsMhs');});
